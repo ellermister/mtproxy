c3f956132c03eb1bd78040f5fccc4eb8371b7ef4
chauncey <ellermister@gmail.com>
2020-02-02 21:03:14 +0800

    Update mtproxy.sh
    修正生成链接

--- mtproxy.sh
+++ mtproxy.sh
@@ -175,8 +175,8 @@ info_mtp(){
     echo -e "服务器IP：\033[31m$public_ip\033[0m"
     echo -e "服务器端口：\033[31m$port\033[0m"
     echo -e "MTProxy Secret:  \033[31m$secret\033[0m"
-    echo -e "TG一键链接: https://t.me/proxy?server=${ip}&port=${port}&secret=${client_secret}"
-    echo -e "TG一键链接: tg://proxy?server=${ip}&port=${port}&secret=${client_secret}"
+    echo -e "TG一键链接: https://t.me/proxy?server=${public_ip}&port=${port}&secret=${client_secret}"
+    echo -e "TG一键链接: tg://proxy?server=${public_ip}&port=${port}&secret=${client_secret}"
   else
     echo -e "TMProxy+TLS代理: \033[33m已停止\033[0m"
   fi
