ea41f987fb78efbc4bed56c3d9e9940b663ca820
chauncey <ellermister@gmail.com>
2020-02-02 22:58:35 +0800

    Update mtproxy.sh
    fixed if bug

--- mtproxy.sh
+++ mtproxy.sh
@@ -193,7 +193,7 @@ run_mtp(){
     nat_ip=$(echo $(ip a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | cut -d "/" -f1))
     public_ip=`curl -s https://api.ip.sb/ip`
     nat_info=""
-    if [ $nat_ip ne $public_ip ];then
+    if [[ $nat_ip != $public_ip ]];then
       nat_info="--nat-info ${nat_ip}:${public_ip}"
     fi
     ./mtproto-proxy -u nobody -p $web_port -H $port -S $secret --aes-pwd proxy-secret proxy-multi.conf -M 1 --domain $domain $nat_info >/dev/null 2>&1 &
@@ -210,9 +210,9 @@ debug_mtp(){
   nat_ip=$(echo $(ip a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | cut -d "/" -f1))
   public_ip=`curl -s https://api.ip.sb/ip`
   nat_info=""
-  if [ $nat_ip ne $public_ip ];then
-    nat_info="--nat-info ${nat_ip}:${public_ip}"
-  fi
+  if [[ $nat_ip != $public_ip ]];then
+      nat_info="--nat-info ${nat_ip}:${public_ip}"
+    fi
   echo "当前正在运行调试模式："
   echo -e "\t你随时可以通过 Ctrl+C 进行取消操作"
   echo " ./mtproto-proxy -u nobody -p $web_port -H $port -S $secret --aes-pwd proxy-secret proxy-multi.conf -M 1 --domain $domain $nat_info"
